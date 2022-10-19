b59de2313cf4fd43dd4685e277551c95ed31d3c2
Cristian Morales <crisdavidmm95.dev@gmail.com>
2022-10-19 15:24:22 -0500

    add tx to execute tranfers

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -13,15 +13,20 @@ const (
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
 
-var testQueries *Queries
+var (
+	testQueries *Queries
+	testDB      *sql.DB
+)
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("cannot connect to the database")
 	}
 
-	testQueries = New(conn)
+	testQueries = New(testDB)
 
 	m.Run()
 }
